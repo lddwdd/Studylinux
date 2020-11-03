e0f109062c727d9915a7e767c68d66670aa2e474
lldwdd <947620853@qq.com>
2020-11-03 15:45:12 +0800

    Fiveth

---
+++ week5/5-1-3.c
@@ -0,0 +1,10 @@
+#include"ch4.h"
+
+int main()
+{
+	for(int i=0;i<1013;i++)
+	printf(" ");
+	printf("hello world!");
+	while(1);
+	return 0;
+}
---
+++ week5/5-1.c
@@ -0,0 +1,8 @@
+#include"ch4.h"
+
+int main()
+{
+	printf("hello world!");
+	while(1);
+	return 0;
+}
